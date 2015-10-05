7a489656a9751f2dbb206416cd187bad815b71f1
Daniel <DanCasSig@gmail.com>
2015-10-04 22:27:53 -0500

    Adds icon to the java app

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        primaryStage.getIcons().add(new Image("/img/icon.gif"));
         Parent root = FXMLLoader.load(getClass().getResource("GMaps.fxml"));
         primaryStage.setTitle("Google Maps");
         primaryStage.setScene(new Scene(root, 1000, 700));
